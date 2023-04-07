09b5e80f9cf5d8c3eff570f37ae242083e332b39
marlisonmourao <bentesmourao@gmail.com>
2023-04-06 23:10:50 -0400

    testing authentication

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,61 +1,76 @@
-import { renderHook, act } from '@testing-library/react-hooks'
-import { AuthProvider, useAuth } from './auth'
+import { renderHook, act } from "@testing-library/react-hooks";
+import { AuthProvider, useAuth } from "./auth";
+import { startAsync } from "expo-auth-session";
+import { signInAsync } from "expo-apple-authentication";
 
 jest.mock("@react-native-async-storage/async-storage", () =>
   require("@react-native-async-storage/async-storage/jest/async-storage-mock")
-)
+);
 
-jest.mock("expo-auth-session", () => {
-  return {
-    AuthSession: () => {
-      return {
-        type: "success",
-        user: {
-          id: "any.email",
-          email: "bentesmourao@gmail.com",
-          name: "Marlison",
-          photo: "any_photo.png",
-        },
-      };
-    },
-  };
-});
+jest.mock("expo-auth-session");
 
 jest.mock("expo-apple-authentication", () => {
   return {
-    AppleAuthentication: () => {
-      return {
-        type: "success",
-        user: {
-          id: "any.email",
-          email: "bentesmourao@gmail.com",
-          name: "Marlison",
-          photo: "any_photo.png",
-        },
-      };
-    },
+    signInAsync: jest.fn(),
   };
 });
 
-describe('Auth Hook', () => {
-  it('should be able to sign in with Apple account existing', async () => {
+
+describe("Auth Hook", () => {
+  it("should be able to sign in with Apple account existing", async () => {
+    const AppleMocked = jest.mocked(signInAsync as any)
+    AppleMocked.mockReturnValueOnce({
+      type: "success",
+      user: {
+        id: "1",
+        email: "bentesmourao@gmail.com",
+        name: "Marlison",
+        photo: "any_photo.png",
+      },
+    });
+
     const { result } = renderHook(() => useAuth(), {
-      wrapper: AuthProvider
-    })
+      wrapper: AuthProvider,
+    });
 
     await act(() => result.current.signInWithApple());
 
     expect(result.current.user).toBeTruthy()
-  })
+  });
 
   it("should be able to sign in with Google account existing", async () => {
+    const googleMocked = jest.mocked(startAsync as any);
+    googleMocked.mockReturnValueOnce({
+      type: "success",
+      user: {
+        id: "1",
+        email: "bentesmourao@gmail.com",
+        name: "Marlison",
+        photo: "any_photo.png",
+      },
+    });
+
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
     await act(() => result.current.signInWithGoogle());
-    console.log(result.current.user)
 
-    expect(result.current.user.email).toBeTruthy()
-  })
-})
+    expect(result.current.user).toBeTruthy();
+  });
+
+  it("user should not connect if cancel authentication with Google", async () => {
+    const googleMocked = jest.mocked(startAsync as any);
+    googleMocked.mockReturnValue({
+      type: "cancel",
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty("id");
+  });
+});
