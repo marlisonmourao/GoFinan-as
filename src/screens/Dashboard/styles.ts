5086fd13ffdf097c92a1fcc89158147937d309ea
marlisonmourao <bentesmourao@gmail.com>
2023-06-16 15:00:21 -0400

    fix marginTop header

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -25,7 +25,7 @@ export const Header = styled.View`
 export const UserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
-  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+  margin-top: ${getStatusBarHeight()}px;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
